4d89e45ff1cfb3459c7ae2cee1f2303104ca1ab7
Jenny Wu <jenny.wu@appian.com>
2021-05-18 15:03:03 -0400

    Ch 11 WIP!

--- app/src/main/java/com/jenny/android/criminalintent/Crime.kt
+++ app/src/main/java/com/jenny/android/criminalintent/Crime.kt
@@ -1,9 +1,12 @@
 package com.jenny.android.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
-data class Crime(val id: UUID = UUID.randomUUID(),
-    var title: String = "",
-    var date: Date = Date(),
-    var isSolved: Boolean = false,
-    var requiresPolice: Boolean = false)
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false,
+                 var requiresPolice: Boolean = false)
